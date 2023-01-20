f167df7da02982663ac3061cae7eef8f03f6bcc2
Rajat24111994 <rajatmohangpt3@gmail.com>
2023-01-20 23:50:33 +0530

    Day4

--- Login/login.js
+++ Login/login.js
@@ -1,21 +1,48 @@
+// let display_Name="";
 
 
 
-let run= document.getElementById("sub");
-run.addEventListener("click",fun);
 
-function fun(e){
-    e.preventDefault;
 
-    let text=document.getElementById("h1");
-    let ema= document.getElementById("email");
-    let pass= document.getElementById("password");
-    let getdata= JSON.parse(localStorage.getItem("login"))||[];
-     getdata.forEach(ele => {
-        if (ema.value==ele.Email && pass.value==ele.Password){
-           text.innerText="Login Successful"
-        }else{
-            text.innerText= "Wrong Credentials"
-        }
-    });
+let email= document.getElementById("email");
+let password = document.getElementById("password");
+document.querySelector("#loginform").addEventListener("submit", (e)=>{
+  e.preventDefault();
+
+  let Getdata = JSON.parse(localStorage.getItem("account-data"))||[];
+  let flag1=false;
+  let flag2=false;
+  Getdata.forEach((element) => {
+    if ((email.value== element.Email) && (password.value== element.Password)){
+   // document.getElementById("login").innerText=element.name;
+      flag1=true;
+      flag2=true;
+      // display_Name=element.Name;
+    }
+    
+    
+  });
+
+  if ( email.value =="" || password.value=="" ){
+    alert("please fill all details")
+}else{
+
+
+
+
+  if (flag1 && flag2){
+      alert("Redirecting to the main page")
+     window.location.href= "home.html"
+  }else{
+    alert("Wrong Credentials")
+  }
+ 
 }
+
+ 
+ 
+  
+});
+
+// let user_Name= localStorage.setItem("UserName", display_Name);
+// console.log(user_Name)
